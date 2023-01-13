0ee67a50b131aa70862c593bfee1e3c1bc6fd5c1
Kumar Prateek <kumarprateek902@gmail.com>
2023-01-13 12:00:41 +0530

    second stage

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 # Uncomment this to pass the first stage
 import socket
+import re
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    connection, address = server_socket.accept() # wait for client
+    data = connection.recv(1024).decode()
+    
+    print(f"{data=}")
+    regex = re.compile(r"\bping\b")
+    if re.findall(regex, data) and len(re.findall(regex, data)) > 0:
+        response = "+PONG\r\n"
+        connection.send(response.encode())
 
 if __name__ == "__main__":
     main()
